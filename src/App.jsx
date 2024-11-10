24511d9638b7fccaa1bd6f177819c7b40fb38ae0
Timothy Otholt <timotholt@gmail.com>
2024-11-09 23:11:16 -0800

    moved images into same folder as App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,14 @@ const App = () => {
     {
       date: '11/12/20',
       title: 'On the Street in Brooklyn',
-      image: './src/public/blog-image-1.jpg',
+    //   image: './src/public/blog-image-1.jpg',
+    image: './blog-image-1.jpg',
       content: 'Cray ipsum dolor sit, amet consectetur adipisicing elit. Beatae laboriosam laborum quae quia alias, sequi iure. Quis consequuntur quidem repudiandae ratione vitae dignissimos a incidunt, mollitia nobis similique voluptate eaque, esse at molestias assumenda optio ipsam. Pariatur eaque asperiores nemo et corporis corrupti architecto nulla ex omnis, temporibus debitis libero quibusdam ut itaque reprehenderit? Ipsum consectetur aliquid odit incidunt. Porro ea eos accusamus perferendis, voluptates fugiat, velit minus iste in nisi, voluptatem ratione dolor! Explicabo dolor tempore atque tempora. Quasi voluptatibus est, ad veritatis debitis maiores earum eos deserunt laborum corporis fuga quos ullam. Voluptatum quam alias magni vero fuga!'
     },
     {
       date: '11/11/20',
       title: 'Vintage in Vogue',
-      image: './src/public/blog-image-2.jpg',
+      image: './public/blog-image-2.jpg',
       content: 'Selfies sunt, dolor sit amet cray ipsum dolor sit, amet consectetur adipisicing elit. Beatae laboriosam laborum quae quia alias, sequi iure. Quis consequuntur quidem repudiandae ratione vitae dignissimos a incidunt, mollitia nobis similique voluptate eaque, esse at molestias assumenda optio ipsam. Pariatur eaque asperiores nemo et corporis corrupti architecto nulla ex omnis, temporibus debitis libero quibusdam ut itaque reprehenderit? Ipsum consectetur aliquid odit incidunt. Porro ea eos accusamus perferendis, voluptates fugiat, velit minus iste in nisi, voluptatem ratione dolor! Explicabo dolor tempore atque tempora. Quasi voluptatibus est, ad veritatis debitis maiores earum eos deserunt laborum corporis fuga quos ullam. Voluptatum quam alias magni vero fuga!'
     }
   ];
